2a3f9c18a528f37c2da009342455fb39e42a3030
Abhay <ap3919529@gmail.com>
2020-12-14 12:17:56 +0530

    add function clickhandler

--- app.js
+++ app.js
@@ -3,3 +3,24 @@ var txtInput = document.querySelector("#text-input");
 var btnTranslate = document.querySelector("#btn-translate");
 
 var outputDiv = document.querySelector("#output");
+
+
+
+
+function clickHandler() {
+
+    
+
+    var inputText = txtInput.value
+
+    fetch(getTranslationURL(inputText))
+
+        .then(response => response.json())
+        .then(json => {
+            var translatedText = json.contents.translated;
+            outputDiv.innerText = translatedText;
+
+
+
+          
+         })
